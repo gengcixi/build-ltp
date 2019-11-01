9d940cc544a28466a19938fbb734029373125f84
Cixi Geng <cixi.geng1@unisoc.com>
2019-11-01 13:47:38 +0800

    package build functioin

--- build-ltp.sh
+++ build-ltp.sh
@@ -1,47 +1,80 @@
 #!/bin/bash
 
 # build requirement tools
-sudo apt-get install make 	
-sudo apt-get install bison flex
+#sudo apt-get install make 	
+#sudo apt-get install bison flex
 
 TOPDIR=$(cd `dirname $0`; pwd)
-OUTPUTDIR=$TOPDIR/output 
-mkdir -p $OUTPUTDIR
+OUTPUT=$TOPDIR/output 
+mkdir -p $OUTPUT
 
-if [ $# != 1 ]; then
-    echo "please use sh opts(arm|arm64)"
-    exit 0
+#if [ $# != 1 ]; then
+#    echo "please use sh opts(arm|arm64)"
+#    exit 0
+#
+#fi
 
+if [ "$1" ] ; then
+	ARCH=$1
+else
+	echo "please appoint the architecture:	"
+	echo "1. arm "
+	echo "2. arm64 "
+	read ar
+	case $ar in
+		arm)
+			ARCH=arm
+			CROSS_COMPILE=arm-linux-gnueabihf-
+			;;
+		arm64)
+			ARCH=arm64
+			CROSS_COMPILE=aarch64-linux-gnu-
+			;;
+		1)
+			ARCH=arm
+			CROSS_COMPILE=arm-linux-gnueabihf-
+			;;
+		2)
+			ARCH=arm64
+			CROSS_COMPILE=aarch64-linux-gnu-
+			;;
+		*)
+			echo "Can't find the architectrue: ${ARCH}"
+			exit 0
+			;;
+	esac
 fi
 
-ARCH=arm
-#CROSS_COMPILE=arm-linux-gnueabi-
-#CROSS_COMPILE=arm-linux-androideabi-
-CROSS_COMPILE=arm-linux-gnueabihf-
 platform=$(echo ${CROSS_COMPILE%%-*})-linux
 
 while [ ! -d ltp ];do
     git clone https://github.com/linux-test-project/ltp.git
 done
 
-cd ltp
-git pull
-version=`git tag |tail -1`
-git checkout -b local ${version}
+function build_ltp()
+{
+	echo "======= start build ltp ======="
+	cd $TOPDIR/ltp
+	make O=${OUTPUT}/ltp distclean
+	make O=${OUTPUT}/ltp autotools
 
-make O=${OUTPUTDIR} distclean
-make O=${OUTPUTDIR} autotools
+	echo ${platform}
+	./configure \
+		AR=${CROSS_COMPILE}ar \
+		CC=${CROSS_COMPILE}gcc \
+		RANLIB=${CROSS_COMPILE}ranlib \
+		STRIP=${CROSS_COMPILE}strip \
+		--build=i686-pc-linux-gnu \
+		--host=${platform} \
+		--target=${platform} \
+		--prefix=${OUTPUT}/ltp \
+		ANDROID=1
 
-./configure \
-     AR=${CROSS_COMPILE}ar \
-     CC=${CROSS_COMPILE}gcc \
-     RANLIB=${CROSS_COMPILE}ranlib \
-     STRIP=${CROSS_COMPILE}strip \
-     --build=i686-pc-linux-gnu \
-     --host=${platform} \
-     --target=${platform} \
-     --prefix=$TOPDIR/output \
-     ANDROID=1
-
-make O=$OUTPUTDIR
-make O=$OUTPUTDIR install
+	make O=${OUTPUT}/ltp -j $JOBS
+	make O=${OUTPUT}/ltp install
+	make O=${LTP_OUTPUT} distclean
+	find ./* -maxdepth 1 -name "conf*" -type d |xargs rm -rf
+	echo "======= build ltp done ======="
+	cd ${TOPDIR}
+}
+build_ltp
